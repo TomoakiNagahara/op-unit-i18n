8db7bde7367df73a12bf455ad055f88a5721828e
Tomoaki -imak5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-02-16 10:12:21 +0900

    New: index.php, autoloader.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * unit-i18n:/index.php
+ *
+ * @creation  2018-02-15
+ * @version   1.0
+ * @package   unit-i18n
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('autoloader.php');
+
+//	...
+return true;
